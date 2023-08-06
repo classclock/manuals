058b31e0813dace275dc359275920160ccf4af7d
Adrian Edwards <adrian@adriancedwards.com>
2023-08-06 12:05:39 -0400

    embed git hash into generated PDF names

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 FILES=$(find * -type f -name '*.tex')
 
+HASH=$(git rev-parse --short HEAD)
+
 SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
 IFS=$'\n'      # Change IFS to newline char
 FILES=($FILES) # split the `names` string into an array by the same name
@@ -9,4 +11,13 @@ for f in "${FILES[@]}";
 do 
 	latexmk -cd -pdf -nobibtex -aux-directory=out/ -outdir=.. -emulate-aux-dir "$f"
 done
-# mv "**/*.pdf" .
+
+echo "$HASH"
+mv "**/*.pdf" .
+
+for i in *.pdf;
+do
+	name=${i%.*}
+	echo "$name"
+	mv "$i" "$name-$HASH.pdf";
+done
